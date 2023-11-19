774025c5d29a59b3d1b9640da75ca9cbefb1c1de
MrTriggerMcFragger <78004809+MrTriggerMcFragger@users.noreply.github.com>
2023-11-18 20:19:27 -0500

    Started integrating comms stack
    + Updated comms stack declaration/initialization
    + Added a packet reception callback
    + Added/updated terminal options
    + Added a ring token watchdog task

---
+++ build/sketch/Blueteeth-Master.ino.cpp
@@ -0,0 +1,244 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+#include "Blueteeth-Master.h"
+
+int scanTime = 5; //In seconds
+char input_buffer[MAX_BUFFER_SIZE];
+BLEScan* pBLEScan;
+SemaphoreHandle_t uartMutex;
+TaskHandle_t terminalInputTaskHandle;
+TaskHandle_t ringTokenWatchdogTaskHandle;
+TaskHandle_t packetReceptionTaskHandle;
+
+terminalParameters_t terminalParameters;
+int discoveryIdx;
+
+BluetoothA2DPSink a2dpSink;
+
+BlueteethMasterStack internalNetworkStack(10, &packetReceptionTaskHandle, &Serial2, &Serial1); //Serial1 = Data Plane, Serial2 = Control Plane
+BlueteethBaseStack * internalNetworkStackPtr = &internalNetworkStack; //Need pointer for run-time polymorphism
+
+// callback 
+#line 20 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+int32_t get_sound_data(Frame *data, int32_t frameCount);
+#line 27 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+void setup();
+#line 65 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+void loop();
+#line 73 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+void ringTokenWatchdogTask(void * params);
+#line 85 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+void packetReceptionTask(void * pvParams);
+#line 135 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+void terminalInputTask(void * params);
+#line 20 "C:\\Users\\ztzac\\Documents\\GitHub\\Blueteeth-Master\\Blueteeth-Master.ino"
+int32_t get_sound_data(Frame *data, int32_t frameCount) {
+    // generate your sound data 
+    // return the effective length (in frames) of the generated sound  (which usually is identical with the requested len)
+    // 1 frame is 2 channels * 2 bytes = 4 bytes
+    return frameCount;
+}
+
+void setup() {
+  
+  //Start Serial comms
+  Serial.begin(115200);
+  uartMutex = xSemaphoreCreateMutex(); //mutex for UART
+
+  internalNetworkStack.begin();
+  
+  //Setup Peripherals
+  pBLEScan = bleScanSetup();
+
+  //State variable initialization
+  terminalParameters.scanIdx = -1;
+
+  //Create tasks
+  xTaskCreate(terminalInputTask, // Task function
+  "UART TERMINAL INPUT", // Task name
+  4096, // Stack size 
+  NULL, 
+  1, // Priority
+  &terminalInputTaskHandle); // Task handler
+  
+  xTaskCreate(ringTokenWatchdogTask, // Task function
+  "RING TOKEN WATCHDOG", // Task name
+  4096, // Stack size 
+  NULL, 
+  1, // Priority
+  &ringTokenWatchdogTaskHandle); // Task handler
+
+  xTaskCreate(packetReceptionTask, // Task function
+  "PACKET RECEPTION HANDLER", // Task name
+  4096, // Stack size 
+  NULL, 
+  1, // Priority
+  &packetReceptionTaskHandle); // Task handler
+
+}
+
+void loop() {
+}
+
+
+//Name: ringTokenWatchdogTask
+//Purpose: Take in user inputs and handle actions.
+//Inputs: None
+//Outputs: None
+void ringTokenWatchdogTask(void * params) {
+  while (1){
+    vTaskDelay(3000);
+    if (internalNetworkStack.getTokenRxFlag() == false){
+      Serial.print("Generating a new token.\n\r"); //DEBUG STATEMENT
+      // internalNetworkStack.tokenReceived();
+      internalNetworkStack.generateNewToken();
+    }
+    internalNetworkStack.resetTokenRxFlag(); 
+  }
+}
+
+void packetReceptionTask (void * pvParams){
+  while(1){
+    
+    vTaskSuspend(packetReceptionTaskHandle);
+    BlueteethPacket packetReceived = internalNetworkStack.getPacket();
+
+    switch(packetReceived.type){
+      
+      case PING:
+        Serial.print("Ping packet type received.\n\r"); //DEBUG STATEMENT
+        Serial.printf("Response from address %s\n\r", packetReceived.payload);
+        break;
+
+      default:
+        Serial.print("Unknown packet type received.\n\r"); //DEBUG STATEMENT
+        break;
+    }
+
+  }
+}
+
+void inline printBuffer(int endPos){
+
+  Serial.print("\0337"); //save cursor positon
+  Serial.printf("\033[%dF", endPos + 1); //go up N + 1 lines
+  for (int i = 0; i <= endPos; i++) {
+
+    Serial.print("\033[2K"); //clear line
+    
+    switch(input_buffer[i]){
+      case '\0':
+        Serial.printf("Character %d = NULL\n\r", i);
+        break;
+      case '\n':
+        Serial.printf("Character %d = NEWLINE\n\r", i);
+        break;
+      case 127:
+        Serial.printf("Character %d = BACKSPACE\n\r", i);
+        break;
+      default:
+        Serial.printf("Character %d = %c\n\r", i , input_buffer[i]);
+    }
+  }
+  Serial.print("\0338"); //restore cursor position
+}
+
+//Name: terminalInputTask
+//Purpose: Take in user inputs and handle actions.
+//Inputs: None
+//Outputs: None
+void terminalInputTask(void * params) {
+
+  clear_buffer(input_buffer, sizeof(input_buffer));
+  int buffer_pos = 0;
+  BLEScanResults scanResults;
+  const char * btTarget;
+  
+  while(1){
+
+    vTaskDelay(100);
+
+    xSemaphoreTake(uartMutex, portMAX_DELAY);
+
+    while (Serial.available() && (buffer_pos < MAX_BUFFER_SIZE)){ //get number of bits on buffer
+      
+      input_buffer[buffer_pos] = Serial.read();
+
+      //handle special chracters
+      if (input_buffer[buffer_pos] == '\r') { //If an enter character is received
+        
+        input_buffer[buffer_pos] = '\0'; //Get rid of the carriage return
+        Serial.print("\n\r");
+        
+        BlueteethPacket newPacket (false, internalNetworkStack.getAddress(), 254); //Need to declare prior to switch statement to avoid "crosses initilization" error.
+
+        switch ( handle_input(input_buffer, terminalParameters) ){
+          case PING:
+            newPacket.type = PING;
+            internalNetworkStack.queuePacket(1, newPacket);
+            break;
+
+          case SCAN:
+            
+            discoveryIdx = 0;
+
+            scanResults = performBLEScan(pBLEScan, 5);
+            vTaskDelay(5 * 1000);
+            
+            Serial.print("\0337"); //save cursor
+            Serial.printf("\033[%dF", scanResults.getCount() + 1); //go up N + 1 lines
+            Serial.print("\0332K"); //clear line
+            Serial.print("*** SCAN RESULTS START ***");
+            Serial.print("\0338"); //restore cursor
+            Serial.print("*** SCAN RESULTS END   ***");
+
+            break;
+
+          case SELECT:
+            if ((terminalParameters.scanIdx > 0) && (terminalParameters.scanIdx < discoveryIdx)){
+              btTarget = scanResults.getDevice(terminalParameters.scanIdx).getName().c_str(); 
+              Serial.printf("Target set to %s\n\r", btTarget);
+            }
+            else {
+              Serial.print("Selection failed\n\r");
+            }
+            break;
+
+          case STREAM:
+            for (int j = 0; j < 156; j++){
+              for (int i = 1; i <= 255; i++){
+                internalNetworkStack.streamData(i);
+              }
+            }
+            for (int i = 0; i < 220; i++){
+              internalNetworkStack.streamData(i);
+            }
+            break;
+            
+          default:
+            break;
+            //no action needed
+
+        } //handle the input
+        clear_buffer(input_buffer, sizeof(input_buffer));
+        buffer_pos = -1; //return the buffer back to zero (incrimented after this statement)
+        // Serial.printf("Buffer pos is %d", buffer_pos);
+      }
+      
+      else if (input_buffer[buffer_pos] == 127){ //handle a backspace character
+        Serial.printf("%c", 127); //print out backspace
+        input_buffer[buffer_pos] = '\0'; //clear the backspace 
+        if (buffer_pos > 0) input_buffer[--buffer_pos] = '\0'; //clear the previous buffer pos if there was another character in the buffer that wasn't a backspace
+        buffer_pos--;
+      }
+      
+      else Serial.printf("%c", input_buffer[buffer_pos]);
+
+      buffer_pos++;
+      
+    }
+
+    xSemaphoreGive(uartMutex);
+
+  }
+}
